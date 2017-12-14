b29ab5185d2a4bf370d8c18133371a8b71c36a17
German Yakimov <german13yakimov@gmail.com>
2017-12-14 21:43:06 +0700

    Добавление позитивных и негативных документов, бета-версия delta TF-IDF для отдельных слов

---
+++ classifier (beta)/beta-classifier.py
@@ -0,0 +1,65 @@
+import math
+import pymorphy2
+from pprint import pprint
+
+
+def lemmatization(string):
+    morph = pymorphy2.MorphAnalyzer()
+
+    punctuation_list = ['.', ',', '?', '!', ':', ';', '/', '&', '*', '%', '@', '#', '+', '=', '>', '<', '{', '}', '[',
+                        ']', '"', "'", '`',
+                        '$', '№', '^', '(', ')']
+
+    for punctuation in punctuation_list:
+        string = string.replace(punctuation, '')
+
+    string = string.split()
+    for num, word in enumerate(string):
+        string[num] = morph.parse(word)[0].normal_form
+
+    string = [word + ' ' for word in string]
+
+    return ''.join(string).strip().lower()
+
+
+with open('negative (beta).txt') as file:
+    negative = [lemmatization(string) for string in file.read().strip().split('\n')]
+
+with open('positive (beta).txt') as file:
+    positive = [lemmatization(string) for string in file.read().strip().split('\n')]
+
+
+def delta_tf_idf(word):
+    morph = pymorphy2.MorphAnalyzer()
+    word = morph.parse(word)[0].normal_form
+    neg_docs = len(negative)
+    pos_docs = len(positive)
+    pos_docs_word = 1
+    neg_docs_word = 1
+    this_doc_word = 1
+
+    for string in positive:
+        count = string.count(word)
+        if count > 0:
+            pos_docs_word += count
+
+    for string in negative:
+        count = string.count(word)
+        if count > 0:
+            neg_docs_word += count
+
+    print(neg_docs)
+    print(pos_docs)
+    print(neg_docs_word)
+    print(pos_docs_word)
+
+    try:
+        v = this_doc_word * math.log10((neg_docs * pos_docs_word) / (pos_docs * neg_docs_word))
+    except:
+        return "I don't know this word"
+
+    return v
+
+
+word = input('Enter the word: ')
+print(delta_tf_idf(word.strip().lower()))
