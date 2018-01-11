eff7e8ab989d9adebae5705f29383f5e3b0504ea
German Yakimov <german13yakimov@gmail.com>
2018-01-11 21:04:28 +0600

    system modules переименован в modules

--- classifier (beta)/beta-classifier.py
+++ classifier (beta)/beta-classifier.py
@@ -1,6 +1,6 @@
 import math
 import pymorphy2
-from system_modules.get_word_info.get_word_info import get_word_info
+from modules.get_word_info.get_word_info import get_word_info
 
 
 def delta_tf_idf(word):
--- classifier (beta)/config_db.py
+++ classifier (beta)/config_db.py
@@ -1,5 +1,5 @@
-from system_modules.add_to_db.add_to_db import add_docs_to_db
-from system_modules.update_db.update_db import update_db
+from modules.add_to_db.add_to_db import add_docs_to_db
+from modules.update_db.update_db import update_db
 
 
 add_docs_to_db()
