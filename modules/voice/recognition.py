bde1d4526d437e2024cfdfb4f5d35445d36006e7
German Yakimov <german13yakimov@gmail.com>
2018-02-01 21:28:16 +0700

    Создан и протестирован модуль для распознавания голоса (recognition.py)

--- modules/voice/recognition.py
+++ modules/voice/recognition.py
@@ -2,14 +2,18 @@ import speech_recognition as sr
 
 r = sr.Recognizer()
 
-with sr.Microphone() as source:
-    print("Скажите что-нибудь")
-    audio = r.listen(source)
+while True:
+    with sr.Microphone() as source:
+        print("Скажите что-нибудь")
+        audio = r.listen(source)
 
-try:
-     print(r.recognize_google(audio, language="ru-RU"))
-except sr.UnknownValueError:
-    print("Робот не расслышал фразу")
-except sr.RequestError as e:
-    print("Ошибка сервиса; {0}".format(e))
-    
+    try:
+        string = r.recognize_google(audio, language="ru-RU").lower()
+        if string == 'стоп':
+            break
+        else:
+            print(string)
+    except sr.UnknownValueError:
+        print("Робот не расслышал фразу")
+    except sr.RequestError as e:
+        print("Ошибка сервиса; {0}".format(e))
