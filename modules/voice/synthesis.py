6451694a875686538d85bd33f5ab9dea825a664a
German Yakimov <german13yakimov@gmail.com>
2018-02-02 10:27:11 +0700

    Доработка модуля синтеза речи, создание функции

--- modules/voice/synthesis.py
+++ modules/voice/synthesis.py
@@ -3,37 +3,36 @@
 # License: https://github.com/GermanYakimov/Text_tone_analyzer/blob/master/LICENSE
 # Contacts: german@yakimov.su, alekseysheboltasov@gmail.com
 
-
 import requests
 from pygame import mixer
 import time
 import os
 
-mixer.init()
 
-text = input('Введите текст: ')
+def synthesize_speech(text):
+    mixer.init()
 
-url = 'https://tts.voicetech.yandex.net/generate'
-key = 'd5ef89bb-c6fb-4376-83bd-b5980e0b443e'
+    url = 'https://tts.voicetech.yandex.net/generate'
+    key = 'd5ef89bb-c6fb-4376-83bd-b5980e0b443e'
 
-with open('speech.mp3', 'wb') as file:
-    response = requests.get(url, params={
-        'text': text,
-        'format': 'mp3',
-        'lang': 'ru-RU',
-        'speaker': 'zahar',
-        'emotion': 'good',
-        'key': key
-    })
+    with open('speech.mp3', 'wb') as file:
+        response = requests.get(url, params={
+            'text': text,
+            'format': 'mp3',
+            'lang': 'ru-RU',
+            'speaker': 'zahar',
+            'emotion': 'good',
+            'key': key
+        })
 
-    file.write(response.content)
+        file.write(response.content)
 
-mixer.music.load('speech.mp3')
-mixer.music.play()
-while mixer.music.get_busy():
-    time.sleep(0.1)
+    mixer.music.load('speech.mp3')
+    mixer.music.play()
+    while mixer.music.get_busy():
+        time.sleep(0.1)
 
-mixer.stop()
-mixer.quit()
+    mixer.stop()
+    mixer.quit()
 
-os.remove('speech.mp3')
+    os.remove('speech.mp3')
