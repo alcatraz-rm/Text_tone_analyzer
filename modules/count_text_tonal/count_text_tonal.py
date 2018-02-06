959fc83e29f2f905c615e0a6a5ddbf70fb32d314
AlekseySheboltasov <alekseysheboltasov@gmail.com>
2018-02-06 19:56:21 +0700

    Добавлен модуль, определяющий вес и тональность текста.

---
+++ modules/count_text_tonal/count_text_tonal.py
@@ -0,0 +1,17 @@
+# Copyright © 2017-2018. All rights reserved.
+# Authors: German Yakimov, Aleksey Sheboltasov
+# License: https://github.com/GermanYakimov/Text_tone_analyzer/blob/master/LICENSE
+# Contacts: german@yakimov.su, alekseysheboltasov@gmail.com
+
+from modules.count_text_weight.count_text_weight import count_text_weight
+from modules.lemmatization.lemmatization import lemmatization
+from modules.classifier.classifier import classifier
+
+def count_text_tonal(text):
+    text = lemmatization(text)
+    weight = count_text_weight(text)
+    if weight == 0:
+        tonal = 'Unknown Tonal'
+    else:
+        tonal = classifier(weight)
+    return (tonal, weight)
