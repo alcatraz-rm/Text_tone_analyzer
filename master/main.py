b63f948d9c35bbb530ac366611582823b336ae1d
German Yakimov <german13yakimov@gmail.com>
2018-02-06 21:36:50 +0700

    Доработан баг с надписью в GUI.

--- master/main.py
+++ master/main.py
@@ -7,6 +7,7 @@
 import sys
 from PyQt5.QtWidgets import QWidget, QLabel, QLineEdit, QApplication, QPushButton
 from modules.count_text_tonal.count_text_tonal import count_text_tonal
+from PyQt5.QtGui import QFont
 
 
 class App(QWidget):
@@ -19,18 +20,20 @@ class App(QWidget):
         self.qle = QLineEdit(self)
         self.qle.resize(350, 30)
         self.qle.move(75, 40)
-
-        self.btn = QPushButton("Ответ", self)
-        self.btn.resize(120, 50)
-        self.btn.move(190, 100)
-        self.btn.clicked.connect(self.button_clicked)
+        self.qle.setFont(QFont("Times", 14))
 
         self.lbl = QLabel(self)
-        self.lbl.move(140, 180)
-        self.lbl.show()
+        self.lbl.move(50, 150)
+        self.lbl.setFont(QFont("Times", 14))
+        self.lbl.resize(300, 100)
 
-        self.setGeometry(500, 500, 500, 500)
-        self.setWindowTitle('Sentiment Analysis')
+        self.btn = QPushButton("Посчитать тональность", self)
+        self.btn.resize(180, 50)
+        self.btn.move(150, 100)
+        self.btn.clicked.connect(self.button_clicked)
+
+        self.setGeometry(500, 500, 500, 300)
+        self.setWindowTitle('Sentiment Analyser')
         self.show()
 
     def button_clicked(self):
