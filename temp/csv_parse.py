3a5957c9ae3fbf2ada498f41df818f6d82d7b3e8
German Yakimov <german13yakimov@gmail.com>
2018-02-22 21:07:35 +0700

    Создана папка temp, добавлено около 30 000 новых документов, создана база из 10 000 нейтральных документов.

---
+++ temp/csv_parse.py
@@ -0,0 +1,47 @@
+import csv
+from modules.count_text_weight.count_text_weight import count_text_weight
+from modules.lemmatization.lemmatization import lemmatization
+
+# with open('neutral (base).csv', 'w') as f:
+#     pass
+#
+#
+# def dump_to_neutral(s):
+#     s[0] = lemmatization(s[0])
+#     # s[1] = str(count_text_weight(s[0]))
+#     with open('neutral (base).csv', 'a', encoding='utf-8') as file:
+#         file.write(s[0] + '\n')
+#
+#
+# def dump_to_negative(s):
+#     s[0] = lemmatization(s[0])
+#     # s[1] = str(count_text_weight(s[0]))
+#     with open('negative (base)_extended.csv', 'a', encoding='utf-8') as file:
+#         file.write(s[0] + '\n')
+#
+#
+# def dump_to_positive(s):
+#     s[0] = lemmatization(s[0])
+#     # s[1] = str(count_text_weight(s[0]))
+#     with open('positive (base)_extended.csv', 'a', encoding='utf-8') as file:
+#         file.write(s[0] + '\n')
+#
+#
+# with open('text_rating_final_exported_fixed.csv', 'r') as file:
+#     reader = csv.reader(file)
+#     for row in reader:
+#         s = ''.join(row).split(';')
+#         print(s)
+#         try:
+#             if int(s[1]) == 0:
+#                 dump_to_neutral(s)
+#             elif int(s[1]) < 0:
+#                 dump_to_negative(s)
+#             elif int(s[1]) > 0:
+#                 dump_to_positive(s)
+#         except:
+#             pass
+
+
+with open('neutral (base).csv', 'r', encoding='utf-8') as file:
+    reader = csv.reader(file)
