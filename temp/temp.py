bb6c5fa4c494d6b16ce55fa2b59c81458afdb0c3
German Yakimov <german13yakimov@gmail.com>
2018-02-24 01:22:33 +0700

    В БД добавлены униграммы из всех позитивных текстов и 58050 негативных текстов (требуется добавить все оставшиеся).

--- temp/temp.py
+++ temp/temp.py
@@ -3,11 +3,12 @@ from pprint import pprint
 from modules.lemmatization.lemmatization import lemmatization
 import sqlite3
 from datetime import datetime
+import copy
 
 
-# conn = sqlite3.connect('unigrams.db')
-# cursor = conn.cursor()
-# changes_date = str(datetime.now())
+conn = sqlite3.connect('unigrams.db')
+cursor = conn.cursor()
+changes_date = str(datetime.now())
 
 
 pos = list()
@@ -31,17 +32,17 @@ with open('neutral.csv', 'r', encoding='utf-8') as file:
         neu.append(''.join(f))
 
 
-# def check_word(text):
-#     request = ("""
-#     SELECT * FROM 'data' WHERE Unigram='%s'
-#     """) % text
-#
-#     cursor.execute(request)
-#     data = cursor.fetchone()
-#     if data:
-#         return True
-#     else:
-#         return False
+def check_word(text):
+    request = ("""
+    SELECT * FROM 'data' WHERE Unigram='%s'
+    """) % text
+
+    cursor.execute(request)
+    data = cursor.fetchone()
+    if data:
+        return True
+    else:
+        return False
 
 
 def count(word):
@@ -64,3 +65,37 @@ def count(word):
 
     return pos_count, neg_count, neu_count
 
+
+def add_to_db(data):
+    cursor.executemany("INSERT INTO [Data] VALUES (?,?,?,?,?)", data)
+    conn.commit()
+
+
+def add_to_data(unigram, data):
+    pos, neg, neu = count(unigram)
+    data.append((unigram, pos, neg, neu, changes_date))
+    data = copy.deepcopy(data)
+
+    return data
+
+
+with open('neutral.csv', 'r', encoding='utf-8') as file:
+    reader = csv.reader(file)
+    k = 0
+    data = list()
+
+    for row in reader:
+        doc = ''.join(row)
+        for unigram in doc.split():
+            if not check_word(unigram):
+                data = add_to_data(unigram, data)
+        k += 1
+        print(k)
+
+        if k % 500 == 0:
+            add_to_db(data)
+            data.clear()
+
+    add_to_db(data)
+
+cursor.close()
