e8da94ed3be50b051e0231f778e93f7472d58aba
German Yakimov <german13yakimov@gmail.com>
2018-03-26 14:43:21 +0700

    Добавлен файл для работы с датасетом, созданы функции для считывания данных в виде двух списков и в виде pandas Dataframe.

---
+++ temp/read_dataset.py
@@ -0,0 +1,39 @@
+import csv
+from pprint import pprint
+import pandas
+
+
+def read_dataset_as_dataframe():
+    dataframe_list = list()
+
+    with open('dataset.csv', 'r', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            data = ''.join(row).split(';')
+            dataframe_list.append([data[2], data[1]])  # temp, for one feature (unigrams weight)
+            # dataframe_list.append([data[2], data[3], data[4], data[1]])
+
+    dataframe = pandas.DataFrame(data=dataframe_list, columns=['unigrams_weight', 'label'])  # temp, for one feature (unigrams weight)
+    # dataframe = pandas.DataFrame(data=dataframe_list, columns=['unigrams_weight', 'bigrams_weight', 'trigrams_weight',
+    #                                                           'label'])
+
+    return dataframe
+
+
+def read_dataset():
+    x = list()
+    y = list()
+
+    with open('dataset.csv', 'r', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            data = ''.join(row).split(';')
+            y.append(data[1])  # appending label
+            x.append([data[2]])  # temp, for one feature (unigrams weight)
+            # x.append([data[2], data[3], data[4]])
+
+    return x, y
+
+
+# x, y = read_dataset()
+# dataframe = read_dataset_as_dataframe()
