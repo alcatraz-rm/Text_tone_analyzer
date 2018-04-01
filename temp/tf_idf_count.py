b9f707fa11c28664acfc24e61005d72f26073fc4
German Yakimov <german13yakimov@gmail.com>
2018-04-01 11:47:51 +0700

    в файлы добавлен copyright

--- temp/tf_idf_count.py
+++ temp/tf_idf_count.py
@@ -1,3 +1,9 @@
+# Copyright © 2017-2018. All rights reserved.
+# Authors: German Yakimov, Aleksey Sheboltasov
+# License: https://github.com/GermanYakimov/Text_tone_analyzer/blob/master/LICENSE
+# Contacts: german@yakimov.su, alekseysheboltasov@gmail.com
+
+
 from modules.get_ngram_info.get_ngram_info import get_ngram_info
 import math
 from modules.count_text_tonal.count_text_tonal import Document
--- tests/system_tester.py
+++ tests/system_tester.py
@@ -1,3 +1,9 @@
+# Copyright © 2017-2018. All rights reserved.
+# Authors: German Yakimov, Aleksey Sheboltasov
+# License: https://github.com/GermanYakimov/Text_tone_analyzer/blob/master/LICENSE
+# Contacts: german@yakimov.su, alekseysheboltasov@gmail.com
+
+
 from modules.count_text_tonal.count_text_tonal import Document
 
 
