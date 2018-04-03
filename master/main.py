082484ce9974a6aecfcdb2f57e8618e0ea1ee065
German Yakimov <german13yakimov@gmail.com>
2018-04-03 21:34:05 +0700

    добавлена проверка на операционную систему

--- master/main.py
+++ master/main.py
@@ -11,6 +11,9 @@ from PyQt5.QtWidgets import QWidget, QLabel, QLineEdit, QApplication, QPushButto
 from PyQt5.QtGui import QFont, QIcon
 from modules.count_text_tonal.count_text_tonal import Document
 from modules.voice.recognition import recognize_speech
+import platform
+
+system = platform.system().lower()
 
 if not os.path.exists('logs'):
     os.mkdir('logs')
@@ -18,6 +21,7 @@ if not os.path.exists('logs'):
 time = str(datetime.datetime.now()).replace(':', '-')
 logging.basicConfig(filename=os.path.join('logs', 'log_%s.log' % time), filemode='w', level=logging.INFO)
 logging.info('\nmain\n')
+logging.info('\noperation system: %s\n' % system)
 
 
 class MainProgramWindow(QWidget):
@@ -29,58 +33,63 @@ class MainProgramWindow(QWidget):
     def main(self):
         self.setWindowIcon(QIcon('icon.ico'))
         self.setWindowTitle('Sentiment Analyser')
-        self.setFixedSize(500, 300)
-        self.setStyleSheet("QWidget { background-color: rgb(255, 222, 200) }")
 
-        self.qle = QLineEdit(self)
-        self.qle.resize(375, 30)
-        self.qle.setStyleSheet("QWidget { background-color: rgb(255, 255, 255) }")
-        self.qle.move(32.5, 40)
-        self.qle.setToolTip('Enter the text here')
-        self.qle.setFont(QFont("Times", 14))
+        if system == 'windows':
+            self.setFixedSize(500, 300)
+            self.setStyleSheet("QWidget { background-color: rgb(255, 222, 200) }")
 
-        self.lbl_answ = QLabel(self)
-        self.lbl_answ.move(180, 180)
-        self.lbl_answ.setFont(QFont("Times", 24))
-        self.lbl_answ.resize(300, 100)
+            self.qle = QLineEdit(self)
+            self.qle.resize(375, 30)
+            self.qle.setStyleSheet("QWidget { background-color: rgb(255, 255, 255) }")
+            self.qle.move(32.5, 40)
+            self.qle.setToolTip('Enter the text here')
+            self.qle.setFont(QFont("Times", 14))
 
-        self.answer_button = QPushButton("Start", self)
-        self.answer_button.setStyleSheet("""
-            QPushButton:hover { background-color: rgb(144, 235, 235) }
-            QPushButton:!hover { background-color: rgb(134, 227, 227) }
-            QPushButton:pressed { background-color: rgb(124, 218, 217); }
-        """)
-        self.answer_button.resize(190, 60)
-        self.answer_button.move(155, 100)
-        self.answer_button.setFont(QFont('Times', 17))
-        self.answer_button.setToolTip('Push to count tonal')
-        self.answer_button.clicked.connect(self.answer_button_clicked)
+            self.lbl_answ = QLabel(self)
+            self.lbl_answ.move(180, 180)
+            self.lbl_answ.setFont(QFont("Times", 24))
+            self.lbl_answ.resize(300, 100)
 
-        self.voice_button = QPushButton("🎙", self)
-        self.voice_button.resize(30, 30)
-        self.voice_button.setFont(QFont('Times', 17))
-        self.voice_button.move(415, 40)
-        self.voice_button.setToolTip('Push to enter the text by speech')
-        self.voice_button.setStyleSheet("""
-            QPushButton:hover { background-color: rgb(177, 137, 255) }
-            QPushButton:!hover { background-color: rgb(172, 132, 250) }
-            QPushButton:pressed { background-color: rgb(155, 118, 245); }
-        """)
-        self.voice_button.clicked.connect(self.voice_button_clicked)
+            self.answer_button = QPushButton("Start", self)
+            self.answer_button.setStyleSheet("""
+                QPushButton:hover { background-color: rgb(144, 235, 235) }
+                QPushButton:!hover { background-color: rgb(134, 227, 227) }
+                QPushButton:pressed { background-color: rgb(124, 218, 217); }
+            """)
+            self.answer_button.resize(190, 60)
+            self.answer_button.move(155, 100)
+            self.answer_button.setFont(QFont('Times', 17))
+            self.answer_button.setToolTip('Push to count tonal')
+            self.answer_button.clicked.connect(self.answer_button_clicked)
 
-        self.delete_button = QPushButton("✗", self)
-        self.delete_button.resize(30, 30)
-        self.delete_button.setFont(QFont('Times', 17))
-        self.delete_button.move(452, 40)
-        self.delete_button.setToolTip('Push to clear text box')
-        self.delete_button.setStyleSheet("""
-                    QPushButton:!hover { background-color: rgb(180, 180, 180) }
-                    QPushButton:hover { background-color: rgb(200, 200, 200) }
-                    QPushButton:pressed { background-color: rgb(160, 160, 160); }
-                """)
-        self.delete_button.clicked.connect(self.delete_button_clicked)
+            self.voice_button = QPushButton("🎙", self)
+            self.voice_button.resize(30, 30)
+            self.voice_button.setFont(QFont('Times', 17))
+            self.voice_button.move(415, 40)
+            self.voice_button.setToolTip('Push to enter the text by speech')
+            self.voice_button.setStyleSheet("""
+                QPushButton:hover { background-color: rgb(177, 137, 255) }
+                QPushButton:!hover { background-color: rgb(172, 132, 250) }
+                QPushButton:pressed { background-color: rgb(155, 118, 245); }
+            """)
+            self.voice_button.clicked.connect(self.voice_button_clicked)
 
-        self.show()
+            self.delete_button = QPushButton("✗", self)
+            self.delete_button.resize(30, 30)
+            self.delete_button.setFont(QFont('Times', 17))
+            self.delete_button.move(452, 40)
+            self.delete_button.setToolTip('Push to clear text box')
+            self.delete_button.setStyleSheet("""
+                        QPushButton:!hover { background-color: rgb(180, 180, 180) }
+                        QPushButton:hover { background-color: rgb(200, 200, 200) }
+                        QPushButton:pressed { background-color: rgb(160, 160, 160); }
+                    """)
+            self.delete_button.clicked.connect(self.delete_button_clicked)
+
+            self.show()
+
+        elif system == 'darwin':
+            pass
 
     def delete_button_clicked(self):
         self.qle.clear()
@@ -112,12 +121,13 @@ class MainProgramWindow(QWidget):
         doc = Document(self.qle.text())
         doc.count_tonal()
 
-        if doc.tonal == 'positive':
-            self.lbl_answ.setStyleSheet("QLabel {color:rgba(0, 200, 100, 255)}")
-            self.lbl_answ.move(193.5, 180)
-        elif doc.tonal == 'negative':
-            self.lbl_answ.setStyleSheet("QLabel {color:rgba(255, 56, 20, 255)}")
-            self.lbl_answ.move(180, 180)
+        if system == 'windows':
+            if doc.tonal == 'positive':
+                self.lbl_answ.setStyleSheet("QLabel {color:rgba(0, 200, 100, 255)}")
+                self.lbl_answ.move(193.5, 180)
+            elif doc.tonal == 'negative':
+                self.lbl_answ.setStyleSheet("QLabel {color:rgba(255, 56, 20, 255)}")
+                self.lbl_answ.move(180, 180)
 
         self.lbl_answ.setToolTip('Tonal and probability')
         self.lbl_answ.setText(doc.tonal.capitalize() + '\n' + str(round(doc.probability * 100, 3)) + '%')
