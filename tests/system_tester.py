7c0c67965fda964731f01c3ad6a61d9e1b671a3b
German Yakimov <german13yakimov@gmail.com>
2018-04-14 22:22:37 +0700

    добавлены протоколы тестирования

--- tests/system_tester.py
+++ tests/system_tester.py
@@ -54,12 +54,12 @@ class TonalTestCase(unittest.TestCase):
         self.test_results['accuracy'] = str(round(self.test_results['passed'] / len(self.cases), 3) * 100) + '%'
         self.test_results['total runtime'] = end_time - start_time
 
-        with open('report_pos_200_ub.json', 'w', encoding='utf-8') as file:
+        with open('report_neg_200_ub.json', 'w', encoding='utf-8') as file:
             json.dump(self.test_results, file, indent=4, ensure_ascii=False)
 
     def read_cases(self):
         self.cases = dict()
-        with open('tests_positive.csv', 'r', encoding='utf-8') as file:
+        with open('tests_negative.csv', 'r', encoding='utf-8') as file:
             reader = csv.reader(file)
             k = 1
             for row in reader:
