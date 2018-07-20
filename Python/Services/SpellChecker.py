4ac24bbc794518a14171f690c57a83ab93dd3392
German Yakimov <german13yakimov@gmail.com>
2018-07-20 12:04:01 +0700

    Added copyright to all files.

--- Python/Services/SpellChecker.py
+++ Python/Services/SpellChecker.py
@@ -1,3 +1,8 @@
+# Copyright © 2017-2018. All rights reserved.
+# Authors: German Yakimov, Aleksey Sheboltasov
+# License: https://github.com/GermanYakimov/Text_tone_analyzer/blob/master/LICENSE
+# Contacts: german@yakimov.su, alekseysheboltasov@gmail.com
+
 import requests
 
 
