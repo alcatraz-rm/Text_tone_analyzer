ab7cc215364b44adc1e679edb8da022b97034777
German Yakimov <german13yakimov@gmail.com>
2018-07-21 21:02:55 +0700

    Fixed bug with paths in Logger.

--- Python/Services/Logger.py
+++ Python/Services/Logger.py
@@ -18,6 +18,7 @@ class Logger(metaclass=Singleton):
         self.platform = platform.system().lower()
         self.start_time = None
         self.configured = False
+        self.path_to_log = None
 
     def configure(self):
         if not os.path.exists(os.path.join('..', 'Logs')):
@@ -25,39 +26,40 @@ class Logger(metaclass=Singleton):
 
         self.start_time = str(datetime.datetime.now()).replace(':', '-')
 
-        with open(os.path.join('logs', 'log_%s.log' % self.start_time), 'w', encoding='utf-8') as log:
+        with open(os.path.join('..', 'Logs', 'log_%s.log' % self.start_time), 'w', encoding='utf-8') as log:
             log.write('Platform: %s\n' % self.platform)
             log.write('CWD: %s\n' % self.cwd)
             log.write('Start time: %s\n' % self.start_time)
 
+        self.path_to_log = os.path.join('..', 'Logs', 'log_%s.log' % self.start_time)
         self.configured = True
 
     def page_break(self):
-        with open(os.path.join('logs', 'log_%s.log' % self.start_time), 'a', encoding='utf-8') as log:
+        with open(self.path_to_log, 'a', encoding='utf-8') as log:
             log.write('\n\n\n')
 
     def debug(self, message, method_name):
-        with open(os.path.join('logs', 'log_%s.log' % self.start_time), 'a', encoding='utf-8') as log:
+        with open(self.path_to_log, 'a', encoding='utf-8') as log:
             log.write('\n%s | DEBUG | %s | %s\n' % (str(datetime.datetime.now()), method_name, message))
 
     def info(self, message, method_name):
-        with open(os.path.join('logs', 'log_%s.log' % self.start_time), 'a', encoding='utf-8') as log:
+        with open(self.path_to_log, 'a', encoding='utf-8') as log:
             log.write('\n%s | INFO | %s | %s\n' % (str(datetime.datetime.now()), method_name, message))
 
     def warning(self, message, method_name):
-        with open(os.path.join('logs', 'log_%s.log' % self.start_time), 'a', encoding='utf-8') as log:
+        with open(self.path_to_log, 'a', encoding='utf-8') as log:
             warning_message = '\n%s | WARNING | %s | %s\n' % (str(datetime.datetime.now()), method_name, message)
             log.write(warning_message)
             print(warning_message)
 
     def error(self, message, method_name):
-        with open(os.path.join('logs', 'log_%s.log' % self.start_time), 'a', encoding='utf-8') as log:
+        with open(self.path_to_log, 'a', encoding='utf-8') as log:
             error_message = '\n%s | ERROR | %s | %s\n' % (str(datetime.datetime.now()), method_name, message)
             log.write(error_message)
             print(error_message)
 
     def fatal(self, message, method_name):
-        with open(os.path.join('logs', 'log_%s.log' % self.start_time), 'a', encoding='utf-8') as log:
+        with open(self.path_to_log, 'a', encoding='utf-8') as log:
             fatal_message = '\n%s | FATAL | %s | %s\n' % (str(datetime.datetime.now()), method_name, message)
             log.write(fatal_message)
             print(fatal_message)
