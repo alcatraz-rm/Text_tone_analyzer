dff92f6e857646e3d5fc88bf0438fe133c16f9c3
German Yakimov <german13yakimov@gmail.com>
2018-09-22 22:02:33 +0700

    Dump config information into config file for Demo.

--- Python/Demo.py
+++ Python/Demo.py
@@ -15,7 +15,9 @@
 
 import platform
 import sys
+import os
 import time
+import json
 from PyQt5.QtGui import QFont, QIcon
 from PyQt5.QtWidgets import QWidget, QLabel, QLineEdit, QApplication, QPushButton, QMessageBox
 sys.path.append('..')
@@ -24,6 +26,7 @@ from Python.Services.Logger import Logger
 from Python.Services.SpeechRecognizer import SpeechRecognizer
 from Python.TextTonalAnalyzer import TextTonalAnalyzer
 from Python.Services.FileReader import FileReader
+from Python.Services.PathService import PathService
 
 
 class MainWindow(QWidget):
@@ -32,16 +35,17 @@ class MainWindow(QWidget):
         self.os = platform.system().lower()
 
         # Services
-        self.speech_recognizer = SpeechRecognizer()
-        self.file_reader = FileReader()
-        self.logger = Logger()
-        self.text_tonal_analyzer = TextTonalAnalyzer('NBC')
+        self._speech_recognizer = SpeechRecognizer()
+        self._file_reader = FileReader()
+        self.__logger = Logger()
+        self._path_service = PathService()
+        self._text_tonal_analyzer = TextTonalAnalyzer('NBC')
 
-        if not self.logger.configured:
-            self.logger.configure()
+        if not self.__logger.configured:
+            self.__logger.configure()
 
-        # Data
-        # self._config =
+        self._config = None
+        self._load_config()
 
         # GUI Elements
         self.line_edit = QLineEdit(self)
@@ -51,6 +55,17 @@ class MainWindow(QWidget):
         self.file_dialog_button = QPushButton(self)
         self.delete_button = QPushButton(self)
         self.message_box = QMessageBox()
+
+    def _load_config(self):
+        path_to_config = os.path.join(self._path_service.path_to_configs, 'demo.json')
+
+        with open(path_to_config, 'r', encoding='utf-8') as file:
+            self._config = json.load(file)
+
+        if self.os == 'windows':
+            self._config = self._config['windows']
+        else:
+            self._config = self._config['darwin']
 
     def configure_main_window(self):
         self.set_base_params()
@@ -62,143 +77,90 @@ class MainWindow(QWidget):
         self.configure_file_dialog_button()
         self.configure_answer_label()
 
-        self.logger.info('Main window was successfully configured.', 'MainWindow.configure_main_window()')
+        self.__logger.info('Main window was successfully configured.', 'MainWindow.configure_main_window()')
 
     def set_base_params(self):
-        if self.os == 'windows':
-            self.setFixedSize(500, 300)
-            self.setStyleSheet('QWidget { background-color: rgb(255, 222, 200) }')
-
-        elif self.os == 'darwin':
-            self.setFixedSize(600, 350)
-            self.setStyleSheet('QWidget { background-color: rgb(255, 230, 210) }')
+        self.setFixedSize(*self._config['size'])
+        self.setStyleSheet('QWidget { background-color: %s }' % self._config['background-color'])
 
     def configure_line_edit(self):
         self.line_edit.setToolTip('Enter the text here')
         self.line_edit.returnPressed.connect(self.answer_button_clicked)
 
-        if self.os == 'windows':
-            self.line_edit.resize(375, 30)
-            self.line_edit.setStyleSheet('QWidget { background-color: rgb(255, 255, 255) }')
-            self.line_edit.move(32.5, 40)
-            self.line_edit.setFont(QFont('Times', 14))
-
-        elif self.os == 'darwin':
-            self.line_edit.resize(460, 40)
-            self.line_edit.setStyleSheet('QWidget { background-color: rgb(255, 255, 255) }')
-            self.line_edit.move(30, 40)
-            self.line_edit.setFont(QFont('Times', 24))
+        self.line_edit.resize(*self._config['line-edit']['size'])
+        self.line_edit.setStyleSheet('QWidget { background-color: %s }' %
+                                     self._config['line-edit']['background-color'])
+        self.line_edit.move(*self._config['line-edit']['coordinates'])
+        self.line_edit.setFont(QFont(*self._config['line-edit']['font']))
 
     def configure_answer_button(self):
         self.answer_button.clicked.connect(self.answer_button_clicked)
         self.answer_button.setText('Start')
         self.answer_button.setToolTip('Push to count tonal')
 
-        if self.os == 'windows':
-            self.answer_button.setStyleSheet("""
-                            QPushButton:hover { background-color: rgb(144, 235, 235) }
-                            QPushButton:!hover { background-color: rgb(134, 227, 227) }
-                            QPushButton:pressed { background-color: rgb(124, 218, 217); }
-                        """)
-            self.answer_button.resize(190, 60)
-            self.answer_button.move(155, 100)
-            self.answer_button.setFont(QFont('Times', 17))
-
-        elif self.os == 'darwin':
-            self.answer_button.setStyleSheet("""
-                QPushButton:hover { background-color: rgb(144, 235, 235) }
-                QPushButton:!hover { background-color: rgb(134, 227, 227) }
-                QPushButton:pressed { background-color: rgb(124, 218, 217); }
-            """)
-            self.answer_button.resize(190, 60)
-            self.answer_button.move(205, 100)
-            self.answer_button.setFont(QFont('Times', 30))
+        self.answer_button.setStyleSheet("""
+                             QPushButton:hover { background-color: %s }
+                             QPushButton:!hover { background-color: %s }
+                             QPushButton:pressed { background-color: %s; }
+                         """ % (self._config['answer-button']['background-color']['hover'],
+                                self._config['answer-button']['background-color']['!hover'],
+                                self._config['answer-button']['background-color']['pressed']))
+        self.answer_button.resize(*self._config['answer-button']['size'])
+        self.answer_button.move(*self._config['answer-button']['coordinates'])
+        self.answer_button.setFont(QFont(*self._config['answer-button']['font']))
 
     def configure_voice_button(self):
         self.voice_button.setText('🎙')
         self.voice_button.setToolTip('Push to enter the text by speech')
         self.voice_button.clicked.connect(self.voice_button_clicked)
 
-        if self.os == 'windows':
-            self.voice_button.resize(30, 30)
-            self.voice_button.setFont(QFont('Times', 17))
-            self.voice_button.move(415, 40)
-            self.voice_button.setStyleSheet("""
-                            QPushButton:hover { background-color: rgb(177, 137, 255) }
-                            QPushButton:!hover { background-color: rgb(172, 132, 250) }
-                            QPushButton:pressed { background-color: rgb(155, 118, 245); }
-                        """)
-
-        elif self.os == 'darwin':
-            self.voice_button.resize(40, 40)
-            self.voice_button.setFont(QFont('Times', 28))
-            self.voice_button.move(500, 40)
-            self.voice_button.setStyleSheet("""
-                QPushButton:hover { background-color: rgb(177, 137, 255) }
-                QPushButton:!hover { background-color: rgb(172, 132, 250) }
-                QPushButton:pressed { background-color: rgb(155, 118, 245); }
-            """)
+        self.voice_button.resize(*self._config['voice-button']['size'])
+        self.voice_button.setFont(QFont(*self._config['voice-button']['font']))
+        self.voice_button.move(*self._config['voice-button']['coordinates'])
+        self.voice_button.setStyleSheet("""
+                             QPushButton:hover { background-color: %s }
+                             QPushButton:!hover { background-color: %s }
+                             QPushButton:pressed { background-color: %s; }
+                         """ % (self._config['voice-button']['background-color']['hover'],
+                                self._config['voice-button']['background-color']['!hover'],
+                                self._config['voice-button']['background-color']['pressed']))
 
     def configure_delete_button(self):
         self.delete_button.setText('✗')
         self.delete_button.setToolTip('Push to clear text box')
         self.delete_button.clicked.connect(self.delete_button_clicked)
 
-        if self.os == 'windows':
-            self.delete_button.resize(30, 30)
-            self.delete_button.setFont(QFont('Times', 17))
-            self.delete_button.move(452, 40)
-            self.delete_button.setStyleSheet("""
-                                    QPushButton:!hover { background-color: rgb(180, 180, 180) }
-                                    QPushButton:hover { background-color: rgb(200, 200, 200) }
-                                    QPushButton:pressed { background-color: rgb(160, 160, 160); }
-                                """)
-
-        elif self.os == 'darwin':
-            self.delete_button.resize(40, 40)
-            self.delete_button.setFont(QFont('Times', 28))
-            self.delete_button.move(545, 40)
-            self.delete_button.setStyleSheet("""
-                        QPushButton:!hover { background-color: rgb(180, 180, 180) }
-                        QPushButton:hover { background-color: rgb(200, 200, 200) }
-                        QPushButton:pressed { background-color: rgb(160, 160, 160); }
-                    """)
+        self.delete_button.resize(*self._config['delete-button']['size'])
+        self.delete_button.setFont(QFont(*self._config['delete-button']['font']))
+        self.delete_button.move(*self._config['delete-button']['coordinates'])
+        self.delete_button.setStyleSheet("""
+                             QPushButton:hover { background-color: %s }
+                             QPushButton:!hover { background-color: %s }
+                             QPushButton:pressed { background-color: %s; }
+                         """ % (self._config['delete-button']['background-color']['hover'],
+                                self._config['delete-button']['background-color']['!hover'],
+                                self._config['delete-button']['background-color']['pressed']))
 
     def configure_file_dialog_button(self):
         self.file_dialog_button.setText('📂')
         self.file_dialog_button.setToolTip('Push to open file')
         self.file_dialog_button.clicked.connect(self.file_dialog_button_clicked)
 
-        if self.os == 'windows':
-            self.file_dialog_button.resize(67, 30)
-            self.file_dialog_button.setFont(QFont('Times', 17))
-            self.file_dialog_button.move(415, 77)
-            self.file_dialog_button.setStyleSheet("""
-                                                QPushButton:!hover { background-color: rgb(181, 225, 174) }
-                                                QPushButton:hover { background-color: rgb(207, 236, 207) }
-                                                QPushButton:pressed { background-color: rgb(145, 210, 144); }
-                                            """)
-
-        elif self.os == 'darwin':
-            self.file_dialog_button.resize(85, 40)
-            self.file_dialog_button.setFont(QFont('Times', 17))
-            self.file_dialog_button.move(500, 85)
-            self.file_dialog_button.setStyleSheet("""
-                                                QPushButton:!hover { background-color: rgb(181, 225, 174) }
-                                                QPushButton:hover { background-color: rgb(207, 236, 207) }
-                                                QPushButton:pressed { background-color: rgb(145, 210, 144); }
-                                            """)
+        self.file_dialog_button.resize(*self._config['file-dialog-button']['size'])
+        self.file_dialog_button.setFont(QFont(*self._config['file-dialog-button']['font']))
+        self.file_dialog_button.move(*self._config['file-dialog-button']['coordinates'])
+        self.file_dialog_button.setStyleSheet("""
+                             QPushButton:hover { background-color: %s }
+                             QPushButton:!hover { background-color: %s }
+                             QPushButton:pressed { background-color: %s; }
+                         """ % (self._config['file-dialog-button']['background-color']['hover'],
+                                self._config['file-dialog-button']['background-color']['!hover'],
+                                self._config['file-dialog-button']['background-color']['pressed']))
 
     def configure_answer_label(self):
-        if self.os == 'windows':
-            self.answer_label.move(180, 180)
-            self.answer_label.setFont(QFont('Times', 24))
-            self.answer_label.resize(300, 100)
-
-        elif self.os == 'darwin':
-            self.answer_label.move(180, 180)
-            self.answer_label.setFont(QFont('Times', 40))
-            self.answer_label.resize(300, 100)
+        self.answer_label.move(*self._config['answer-label']['coordinates'])
+        self.answer_label.setFont(QFont(*self._config['answer-label']['font']))
+        self.answer_label.resize(*self._config['answer-label']['size'])
 
     def launch(self):
         self.setWindowIcon(QIcon('icon.ico'))
@@ -207,7 +169,7 @@ class MainWindow(QWidget):
         self.configure_main_window()
         self.show()
 
-        self.logger.info('Main window was successfully launched.', 'MainWindow.launch()')
+        self.__logger.info('Main window was successfully launched.', 'MainWindow.launch()')
 
     def delete_button_clicked(self):
         self.line_edit.clear()
@@ -216,7 +178,7 @@ class MainWindow(QWidget):
     def voice_button_clicked(self):
         self.message_box.question(self, 'Speak', 'You can start speeking.', QMessageBox.Ok)
 
-        voice_text = self.speech_recognizer.recognize_speech()
+        voice_text = self._speech_recognizer.recognize_speech()
 
         if voice_text == 'Unknown value':
             try_again = QMessageBox.Yes
@@ -227,7 +189,7 @@ class MainWindow(QWidget):
                 if try_again == QMessageBox.No:
                     break
 
-                voice_text = self.speech_recognizer.recognize_speech()
+                voice_text = self._speech_recognizer.recognize_speech()
 
         if voice_text == 'Internet connection lost':
             self.message_box.question(self, 'Error', 'Internet connection lost', QMessageBox.Ok)
@@ -243,41 +205,38 @@ class MainWindow(QWidget):
             return None
 
     def file_dialog_button_clicked(self):
-        file_content = self.file_reader.get_file_content()
+        file_content = self._file_reader.get_file_content()
         if file_content:
             self.line_edit.setText(file_content)
 
     def answer_button_clicked(self):
-        start_time = time.time()
-        self.text_tonal_analyzer.detect_tonal(self.line_edit.text())
+        self._text_tonal_analyzer.detect_tonal(self.line_edit.text())
 
         if self.os == 'windows':
-            if self.text_tonal_analyzer.tonal == 'positive':
+            if self._text_tonal_analyzer.tonal == 'positive':
                 self.answer_label.setStyleSheet('QLabel {color:rgba(0, 200, 100, 255)}')
                 self.answer_label.move(193.5, 180)
 
-            elif self.text_tonal_analyzer.tonal == 'negative':
+            elif self._text_tonal_analyzer.tonal == 'negative':
                 self.answer_label.setStyleSheet('QLabel {color:rgba(255, 56, 20, 255)}')
                 self.answer_label.move(180, 180)
 
         elif self.os == 'darwin':
-            if self.text_tonal_analyzer.tonal == 'positive':
+            if self._text_tonal_analyzer.tonal == 'positive':
                 self.answer_label.setStyleSheet('QLabel {color:rgba(0, 200, 100, 255)}')
                 self.answer_label.move(230, 210)
 
-            elif self.text_tonal_analyzer.tonal == 'negative':
+            elif self._text_tonal_analyzer.tonal == 'negative':
                 self.answer_label.setStyleSheet('QLabel {color:rgba(255, 56, 20, 255)}')
                 self.answer_label.move(225, 210)
 
         self.answer_label.setToolTip('Tonal and probability')
 
-        if self.text_tonal_analyzer.probability:
-            self.answer_label.setText(self.text_tonal_analyzer.tonal.capitalize() + '\n' +
-                                      str(round(self.text_tonal_analyzer.probability * 100, 3)) + '%')
+        if self._text_tonal_analyzer.probability:
+            self.answer_label.setText(self._text_tonal_analyzer.tonal.capitalize() + '\n' +
+                                      str(round(self._text_tonal_analyzer.probability * 100, 3)) + '%')
         else:
-            self.answer_label.setText(self.text_tonal_analyzer.tonal.capitalize())
-
-        print(time.time() - start_time)
+            self.answer_label.setText(self._text_tonal_analyzer.tonal.capitalize())
 
 
 def get_mode():
